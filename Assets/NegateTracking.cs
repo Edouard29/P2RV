f73d98a831d1dc4ad73e13ab13f7fb967ffc1ab4
Edouard <ferrand_edouard@orange.fr>
2019-02-07 11:22:53 +0100

    First commit

---
+++ Assets/NegateTracking.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class NegateTracking : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        Debug.Log(transform.position);
+    }
+}
---
+++ Assets/RedirectedWalking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedirectedWalking : MonoBehaviour
+{
+    float rotationMultiplicator = 1.1f;
+    public GameObject originalCamera;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+    }
+}
